749a11c697c2bf4542f9ba2fca891f60bdbd34ff
= <=>
2020-04-15 10:41:16 +0200

    Initial commit

---
+++ KPA.Database.Abstractions/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPA.Database.Abstractions
+{
+    public interface IRepository<T>
+    {
+        Task<List<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<List<T>> FindAll(Expression<Func<T, bool>> predicate);
+        Task<T> Find(Expression<Func<T, bool>> predicate);
+        Task<T> Create(T entity);
+        Task<T> Update(T entity);
+        Task Delete(T entity);
+    }
+}
---
+++ KPA.Database.Abstractions/Repository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KPA.Database.Abstractions
+{
+    public abstract class Repository<TContext, TEntity> : IRepository<TEntity> where TContext : DbContext where TEntity : class
+    {
+
+        private readonly TContext context;
+
+        public Repository(TContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<TEntity> Create(TEntity entity)
+        {
+            var added = context.Set<TEntity>().Add(entity);
+            await context.SaveChangesAsync();
+            return added.Entity;
+        }
+
+        public async Task Delete(TEntity entity)
+        {
+            var deleted = context.Set<TEntity>().Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public Task<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return context.Set<TEntity>().FirstOrDefaultAsync(predicate, CancellationToken.None);
+        }
+
+        public Task<List<TEntity>> FindAll(Expression<Func<TEntity, bool>> predicate)
+        {
+            return context.Set<TEntity>().Where(predicate).ToListAsync();
+        }
+
+        public Task<List<TEntity>> GetAll()
+        {
+            return context.Set<TEntity>().ToListAsync();
+        }
+
+        public Task<TEntity> GetById(int id)
+        {
+            return context.Set<TEntity>().FindAsync(id).AsTask();
+        }
+
+        public async Task<TEntity> Update(TEntity entity)
+        {
+            var updated = context.Set<TEntity>().Attach(entity);
+            await context.SaveChangesAsync();
+            return updated.Entity;
+        }
+    }
+}
